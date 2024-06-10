d99b668834cdffb33f4dd3e4133e16f058a969aa
Vinit Damania <vinitdamania@gmail.com>
2024-06-10 22:45:51 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("hello");
 let audioElement = new Audio('songs/1.mp3');
-let songIndex = 0;
+let songIndex = 1;
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
 let songItems = Array.from(document.getElementsByClassName('songItem'))
+let masterSongName = document.getElementById('masterSongName');
+let songItemPlays = Array.from(document.getElementsByClassName('songItemPlay'));
 
 let songs = [
     {songName : "Kya Mujhe Pyaar Hai", filePath: "songs/1.mp3", coverPath : "covers/1.jpg"},
@@ -55,10 +57,80 @@ const makeAllPlays = ()=>{
     })
 }
 
-Array.from(document.getElementsByClassName('songs')).forEach((element)=>{
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         makeAllPlays();
-        e.target.classList.remove('fa-circle-play');
-        e.target.classList.add('fa-circle-pause');
+        songIndex = parseInt(e.target.id);
+        masterSongName.innerText = songs[songIndex - 1].songName;
+        if (audioElement.paused || audioElement.currentTime <=0){
+            e.target.classList.remove('fa-circle-play');
+            e.target.classList.add('fa-circle-pause');
+            audioElement.src = `songs/${songIndex}.mp3`;
+            audioElement.currentTime = 0;
+            audioElement.play();
+            masterPlay.classList.remove('fa-circle-play');
+            masterPlay.classList.add('fa-circle-pause');
+            gif.style.opacity = 1;
+        }
+        else{
+            audioElement.pause();
+            e.target.classList.remove('fa-circle-pause');
+            e.target.classList.add('fa-circle-play');
+            masterPlay.classList.remove('fa-circle-pause');
+            masterPlay.classList.add('fa-circle-play');
+            gif.style.opacity = 0;
+        }
+        
     })
 })
+
+document.getElementById('forward').addEventListener('click', ()=>{
+    if(songIndex >= 5){
+        songIndex = 1;
+    }
+    else{
+            songIndex+=1;
+        }
+        makeAllPlays();
+        updatePlayIcon(songIndex);
+        audioElement.src = `songs/${songIndex}.mp3`;
+        masterSongName.innerText = songs[songIndex - 1].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+})
+
+document.getElementById('backward').addEventListener('click', ()=>{
+    if(songIndex <= 1){
+        songIndex = 5;
+    }
+    else{
+            songIndex-=1;
+        }
+        makeAllPlays();
+        updatePlayIcon(songIndex);
+        audioElement.src = `songs/${songIndex}.mp3`;
+        masterSongName.innerText = songs[songIndex - 1].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+})
+
+
+const updatePlayIcon = (index)=>{
+    songItemPlays.forEach((element,i)=>{
+        if(i+1 == index){
+            element.classList.remove('fa-circle-play');
+            element.classList.add('fa-circle-pause');
+        }
+        else{
+            element.classList.remove('fa-circle-pause');
+            element.classList.add('fa-circle-play');
+        }
+    })
+}
+
